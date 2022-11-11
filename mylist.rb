f350fdc99b3fa21381beb533c58c8e634e7f65c5
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-11 21:49:50 +0530

    Add enumerable all any filter methods functionality

--- mylist.rb
+++ mylist.rb
@@ -1,11 +1,20 @@
+require_relative 'own_enumerable'
+
 class MyList
+  include MyEnum
   def initialize(*list)
     @list = list
   end
-
-  include MyEnumerable
 
   def each(&block)
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4, 5)
+p(list.all?(&:positive?))
+p(list.all? { |item| item > 1 })
+p(list.any?(&:positive?))
+p(list.any? { |item| item > 1 })
+p(list.filter { |item| item > 1 })
+p(list.filter { |item| item > 2 })
